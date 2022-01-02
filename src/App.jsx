b100c56a3a8b91dde8552006090243ca17882e08
de Oliveira Staudt <lucas.de.oliveira.staudt@hp.com>
2022-01-02 00:04:16 -0300

    refactor: rename project in docker and add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
 import React from 'react';
+
+// import LineChart from './components/lineChart/LineChart';
 
 function App() {
   return (
     <div>
       <p>salve</p>
-      <p>salve2</p>
     </div>
   );
 }
